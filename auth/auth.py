d69e7eda6a49b21f6ac9c41a4078e247c8ad38a0
temirovuz <temirov-muhammad@mail.ru>
2024-04-18 16:36:23 +0500

    User Crud function

--- auth/auth.py
+++ auth/auth.py
@@ -9,7 +9,7 @@ from auth.services import crate_access_token
 from core.database import get_db
 from core.ultis import verify_password, hash_password
 
-router = APIRouter(prefix='/auth',tags=["auth"])
+router = APIRouter(prefix='/auth', tags=["auth"])
 
 
 @router.post('/signup', status_code=status.HTTP_201_CREATED)
@@ -17,17 +17,6 @@ def create_users(user_data: CreateUser, db: Session = Depends(get_db)):
     password = hash_password(user_data.password)
     user = create_user(user_data.email, password, db)
     return user
-
-
-# @router.post('/login', status_code=status.HTTP_200_OK, response_model=Token)
-# def signin(user_data: CreateUser, db: Session = Depends(get_db)):
-#     user = db.query(User).filter(User.email == user_data.email).first()
-#     if not user:
-#         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User not found')
-#     if not verify_password(user_data.password, user.password):
-#         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Invalid password')
-#     access_token = crate_access_token(data={'user_id': user.id})
-#     return {'access_token': access_token, 'token_type': 'Bearer'}
 
 
 @router.post("/login", status_code=200, response_model=Token)
@@ -38,7 +27,5 @@ def login(user: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get
         raise HTTPException(status_code=409, detail="Incorrect email")
     if not verify_password(user.password, query.password):
         raise HTTPException(status_code=409, detail="Incorrect password")
-
     access_token = crate_access_token(data={'user_id': query.id})
-
     return {'access_token': access_token, 'token_type': 'Bearer'}
--- auth/router.py
+++ auth/router.py
@@ -1,12 +1,50 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Form
 from jose import jwt, JWTError
 from sqlalchemy.orm import Session
 
 from auth.models import User
-from auth.schemas import UpdateUser, CreateUsername
-from auth.services import SECRET_KEY, ALGORITHM
+from auth.schemas import UpdateUser
+from auth.services import get_current_user
 from core.database import get_db
-
+from core.ultis import verify_password, hash_password
+from posts.schemas import UserOutput
 
 router = APIRouter(prefix="/user", tags=["user"])
 
+
+@router.post("/info")
+def info_user(db=Depends(get_db), user: UserOutput = Depends(get_current_user)):
+    return db.query(User).filter(User.id == user.id).first()
+
+
+@router.post("/update_username")
+def add_username(data=Form(), user: UserOutput = Depends(get_current_user), db=Depends(get_db)):
+    username = db.query(User).filter(User.username == data.lower()).first()
+    if username:
+        raise HTTPException(status_code=400, detail="Bu Username band. Iltimos boshqa username kiriting!")
+    db.query(User).filter(User.id == user.id).update({'username': data.lower()})
+    db.commit()
+    return {"message": "Username qoyildi"}
+
+
+@router.put("/update_password")
+def password_change(pwd=Form(), pwd2=Form(), user: UserOutput = Depends(get_current_user), db=Depends(get_db)):
+    query = db.query(User).filter(User.id == user.id).first()
+    if pwd != pwd2:
+        raise HTTPException(status_code=400, detail="Parollar bir ekanligini tekshirib koring")
+    else:
+        ans = verify_password(pwd, query.password)
+        if ans:
+            raise HTTPException(status_code=400, detail='Yangi kiritgan kodingiz oldingi koddan farql bolishi lozim')
+    up_password = hash_password(pwd)
+    db.query(User).filter(User.id == user.id).update({'password': up_password})
+    db.commit()
+    return {"message": "Password ozgartirildi"}
+
+
+@router.put("/update_fullname")
+def update_fullname(last_name=Form('Familiyangiz'), first_name=Form('Ismingiz'), db=Depends(get_db),
+                    user: UserOutput = Depends(get_current_user)):
+    db.query(User).filter(User.id == user.id).update({'first_name': first_name, 'last_name': last_name})
+    db.commit()
+    return {"message": "Fullname ozgartirildi"}
